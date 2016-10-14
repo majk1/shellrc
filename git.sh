23fa34633ef30c6dd2efd31b9db693086ee3244b
Majki <majki@majki.org>
2016-10-14 16:12:44 +0200

    git and idea project prompt flag

--- git.sh
+++ git.sh
@@ -1,7 +1,12 @@
 
 # Git specific
 
+if [ -z "$SHOW_GIT_BRANCH_PROMPT" ]; then
+    export SHOW_GIT_BRANCH_PROMPT=1
+fi
+
 function _git_ps1() {
+    [ $SHOW_GIT_BRANCH_PROMPT -eq 0 ] && return 1
 	_git_branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 	if [ $? -eq 0 ]; then
 		echo -ne " (${_git_branch})"
--- idea.sh
+++ idea.sh
@@ -1,36 +1,43 @@
 
 # IntelliJ IDEA
 
+if [ -z "$SHOW_IDEA_PROJECT_PROMPT" ]; then
+    export SHOW_IDEA_PROJECT_PROMPT=1
+fi
+
 ps aux | grep $PPID | grep -q 'IntelliJ IDEA'
 export TERMINAL_IN_IDEA=$?
 
 getAbsolutePath () {
     if [ -z "$1" ]; then
-        echo "/";
-        return 0;
-    fi;
-    RELATIVE_DIR="$1";
+        echo "/"
+        return 0
+    fi
+    RELATIVE_DIR="$1"
     if [ -f "$1" ]; then
-        RELATIVE_DIR="$(dirname "${RELATIVE_DIR}")";
-    fi;
-    echo "$(cd "${RELATIVE_DIR}"; pwd)";
+        RELATIVE_DIR="$(dirname "${RELATIVE_DIR}")"
+    fi
+    echo "$(cd "${RELATIVE_DIR}"; pwd)"
     return 0
 }
 
 # TODO: fix this
 getProjectNameInDir () {
-    DIR="$(getAbsolutePath "$1")";
+    [ $SHOW_IDEA_PROJECT_PROMPT -eq 0 ] && return 1
+    DIR="$(getAbsolutePath "$1")"
     if [ -d "${DIR}/.idea" -a -f "${DIR}/.idea/.name" ]; then
-        echo -n "{$(cat "${DIR}/.idea/.name")} ";
-        return 0;
+        echo -n "{$(cat "${DIR}/.idea/.name")} "
+        return 0
+    elif [ -e *.iml ]; then
+        echo -n "{$(echo *.iml | head -n 1 | sed 's/.iml$//')} "
     else
         if [ "${DIR}" != "/" ]; then
-            getProjectNameInDir "${DIR%/*}";
-            return $?;
+            getProjectNameInDir "${DIR%/*}"
+            return $?
         else
-            return 1;
-        fi;
-    fi;
+            return 1
+        fi
+    fi
     return 1
 }
 
