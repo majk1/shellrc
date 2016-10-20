e2875e4cce972b5076aff6b7b4d0888ac843211b
Majki <majki@majki.org>
2016-10-20 10:22:21 +0200

    added clearCache function to linux.sh

--- linux.sh
+++ linux.sh
@@ -1,3 +1,29 @@
 
 alias ls='ls --color'
 
+function clearCache() {
+	local MODE
+	MODE=3
+	if [ ! -z "$1" ]; then
+		if [ "$1" = "-h" ]; then
+			echo "usage: clearCache [mode]"
+			echo "modes:"
+			echo "       1 - pagecache only"
+			echo "       2 - dentries and inodes"
+			echo "       3 - pagecache, dentries and inodes"
+		elif [ "$1" = "1" -o "$1" = "2" -o "$1" = "3" ]; then
+			MODE=$1
+		else
+			echo "Unkown mode: $1"
+			return 1
+		fi
+	fi
+	sync
+	if type -p sudo >/dev/null 2>/dev/null; then
+		echo "echo $MODE > /proc/sys/vm/drop_caches" | sudo sh
+	else
+		su - -c "echo $MODE > /proc/sys/vm/drop_caches"
+	fi
+	return 0
+}
+
