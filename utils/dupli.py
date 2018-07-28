062a3f6b33d10802fde4a55e826dc3886f319439
Majki <majki@majki.org>
2018-07-28 20:12:02 +0200

    dupli.py fix

--- utils/dupli.py
+++ utils/dupli.py
@@ -4,7 +4,6 @@ import sys
 import os
 import hashlib
 
-
 BUF_SIZE = 65536
 
 
@@ -14,8 +13,8 @@ class DuplFile:
         self.name = name
         self.size = os.stat(self.path).st_size
         self.hash = ''
-    
-    def calculateHash(self):
+
+    def calculate_hash(self):
         sha1 = hashlib.sha1()
         with open(self.path, 'rb') as f:
             while True:
@@ -30,79 +29,82 @@ class Duplicates:
     def __init__(self):
         self.files = []
 
-    def addFile(self, duplFile):
-        self.files.append(duplFile)
+    def add_file(self, dupl_file):
+        self.files.append(dupl_file)
 
     def size(self):
         return len(self.files)
 
-    def _byChecksum(self, duplFiles):
-        checksumMap = {}
-        for f in duplFiles:
-            f.calculateHash()
-            if f.hash in checksumMap:
-                hashEqDuplFiles = checksumMap[f.hash]
+    def _by_checksum(self, dupl_files):
+        checksum_map = {}
+        for f in dupl_files:
+            f.calculate_hash()
+            if f.hash in checksum_map:
+                hash_eq_dupl_files = checksum_map[f.hash]
             else:
-                hashEqDuplFiles = []
-            hashEqDuplFiles.append(f)
-            checksumMap[f.hash] = hashEqDuplFiles
-        return checksumMap
+                hash_eq_dupl_files = []
+            hash_eq_dupl_files.append(f)
+            checksum_map[f.hash] = hash_eq_dupl_files
+        return checksum_map
 
-    def _bySize(self):
-        sizeMap = {}
+    def _by_size(self):
+        size_map = {}
         for f in self.files:
-            if f.size in sizeMap:
-                sizeFiles = sizeMap[f.size]
+            if f.size in size_map:
+                size_files = size_map[f.size]
             else:
-                sizeFiles = []
-            sizeFiles.append(f)
-            sizeMap[f.size] = sizeFiles
-        return sizeMap
+                size_files = []
+            size_files.append(f)
+            size_map[f.size] = size_files
+        return size_map
 
-    def findDuplicates(self):
-        sizeMap = self._bySize()
+    def find_duplicates(self):
+        size_map = self._by_size()
         dupls = []
-        for _, files in sizeMap.items():
-            if (len(files) > 1):
-                checkedDups = []
-                hashMap = self._byChecksum(files)
-                for _, chkFiles in hashMap.items():
-                    if (len(chkFiles) > 1):
-                        checkedDups.extend(chkFiles)
-                if len(checkedDups) > 1:
-                    dupls.append(checkedDups)
+        for _, files in size_map.items():
+            if len(files) > 1:
+                checked_dups = []
+                hash_map = self._by_checksum(files)
+                for _, chkFiles in hash_map.items():
+                    if len(chkFiles) > 1:
+                        checked_dups.extend(chkFiles)
+                if len(checked_dups) > 1:
+                    dupls.append(checked_dups)
         return dupls
 
 
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-def findDuplicates(root_path):
+
+def find_and_list_duplicates(root):
     duplicates = Duplicates()
-    for root, _, filenames in os.walk(root_path):
+    for root, _, filenames in os.walk(root):
         for filename in filenames:
-            if (filename != '.DS_Store'):
-                duplicates.addFile(DuplFile(root, filename))
-    dups = duplicates.findDuplicates()
-    
+            if filename != '.DS_Store':
+                duplicates.add_file(DuplFile(root, filename))
+    dups = duplicates.find_duplicates()
+
     print('Listing duplicates:')
 
-    saveSize = 0
+    save_size = 0
     for dup in dups:
         print(f'\nDuplicate files with sha1:{dup[0].hash} and size {dup[0].size}')
-        saveSize += dup[0].size * (len(dup) - 1)
-        for dFile in dup:
-            print(f' - {dFile.path}')
+        save_size += dup[0].size * (len(dup) - 1)
+        for d_file in dup:
+            print(f' - {d_file.path}')
 
-    print(f'\nFound {len(dups)}, storage waste is {sizeof_fmt(saveSize)}')
+    print(f'\nFound {len(dups)}, storage waste is {sizeof_fmt(save_size)}')
 
 
 if __name__ == '__main__':
+
     root_path = '.'
-    if (len(sys.argv) > 1):
+    if len(sys.argv) > 1:
         root_path = sys.argv[1]
-    findDuplicates(root_path)
+
+    find_and_list_duplicates(root_path)
